3591702e8dafb946e94a7e9b904f424a11387c74
philostene <gspillette@gmail.com>
2017-10-10 16:55:33 +0200

    new modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PremierComponent } from './app.monpremiercomponent';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, PremierComponent]
 })
 export class AppModule { }
---
+++ src/app/app.monpremiercomponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-premier',
+  templateUrl: './app.monpremiercomponent.html',
+  styleUrls: ['./app.monpremiercomponent.css']
+})
+export class PremierComponent {
+  title = 'premier';
+}
